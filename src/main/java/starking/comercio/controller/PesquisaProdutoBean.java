c77bc09eecf51b65eafbdf6143fc894e7afeed56
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-01-17 19:58:41 -0300

    exemplo de pesquisar

--- src/main/java/starking/comercio/controller/PesquisaProdutoBean.java
+++ src/main/java/starking/comercio/controller/PesquisaProdutoBean.java
@@ -1,8 +1,28 @@
 package starking.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 /**
  * @author pedroRhamon
  */
+
+@ManagedBean
+@RequestScoped
 public class PesquisaProdutoBean {
 
+	private List<Integer> produtosFiltrados = new ArrayList<>();
+	
+	public PesquisaProdutoBean() {
+		for(int i = 0; i<50; i++) {
+			produtosFiltrados.add(i);
+		}
+	}
+	
+	public List<Integer> getProdutosFiltrados() {
+		return produtosFiltrados;
+	}
 }
