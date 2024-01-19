7a40eed0513c3f505cfd968c4a7334713d10111d
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-01-19 01:55:13 -0300

    adicionando o log4j e o commons

--- src/main/java/starking/comercio/util/jsf/JsfExceptionHandler.java
+++ src/main/java/starking/comercio/util/jsf/JsfExceptionHandler.java
@@ -12,12 +12,17 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import starking.comercio.service.NegocioException;
 
 /**
  * @author pedroRhamon
  */
 public class JsfExceptionHandler extends ExceptionHandlerWrapper {
+	
+	private static Log log = LogFactory.getLog(JsfExceptionHandler.class);
 
 	private ExceptionHandler wrapped;
 	
@@ -52,6 +57,7 @@ public class JsfExceptionHandler extends ExceptionHandlerWrapper {
 					FacesUtil.addErrorMessage(negocioException.getMessage());
 				} else {
 					handled = true;
+					log.error("Erro de sistema: " + exception.getMessage(), exception);
 					redirect("/Erro.xhtml");
 				}
 			} finally {
