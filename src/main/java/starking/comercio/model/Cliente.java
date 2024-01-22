435b1c62846750cbdd5312a5fe2b6061d370a1d7
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-01-22 12:06:40 -0300

    configurando entidade Cliente e Endereco

--- src/main/java/starking/comercio/model/Cliente.java
+++ src/main/java/starking/comercio/model/Cliente.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,6 +30,7 @@ import starking.comercio.model.enums.TipoPessoa;
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "cliente")
 public class Cliente implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -33,12 +39,21 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
+	
+	@Column(nullable = false, length = 100)
 	private String nome;
+	
+	@Column(nullable = false, length = 255)
 	private String email;
+	
+	@Column(name = "doc_Receita_Federal", nullable = false, length = 14)
 	private String docmentoReceitaFederal;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 10)
 	private TipoPessoa tipoPessoa;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade =CascadeType.ALL)
 	private List<Endereco> endereco = new ArrayList<>();
 
 }
--- src/main/java/starking/comercio/model/Endereco.java
+++ src/main/java/starking/comercio/model/Endereco.java
@@ -2,11 +2,14 @@ package starking.comercio.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,6 +24,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "endereco")
 public class Endereco implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -29,14 +33,27 @@ public class Endereco implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
+	
+	@Column(nullable = false, length = 150)
 	private String logradouro;
+	
+	@Column(nullable = false, length = 20)
 	private String numero;
+	
+	@Column(length = 150)
 	private String complemento;
+	
+	@Column(nullable = false, length = 60)
 	private String cidade;
+	
+	@Column(nullable = false, length = 60)
 	private String uf;
+	
+	@Column(nullable = false, length = 9)
 	private String cpf;
 	
 	@ManyToOne
+	@JoinColumn(name = "cliente_id", nullable = false)
 	private Cliente cliente;
 
 }
