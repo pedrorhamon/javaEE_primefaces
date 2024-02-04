0d6fa31389226a0ab91330afc6c681a9577ad492
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-04 18:14:12 -0300

    adicionado as class em outras class

--- src/main/java/starking/comercio/controller/CadastroPedidoBean.java
+++ src/main/java/starking/comercio/controller/CadastroPedidoBean.java
@@ -1,45 +1,78 @@
 package starking.comercio.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import starking.comercio.model.Cliente;
 import starking.comercio.model.EnderecoEntrega;
 import starking.comercio.model.Pedido;
+import starking.comercio.model.Usuario;
+import starking.comercio.model.enums.FormaPagamento;
+import starking.comercio.repository.ClienteRepository;
+import starking.comercio.repository.UsuarioRepository;
+import starking.comercio.service.CadastroPedidoService;
+import starking.comercio.util.jsf.FacesUtil;
 
 /**
  * @author pedroRhamon
  */
-
 @Named
 @ViewScoped
 public class CadastroPedidoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Pedido pedido;
+	@Inject
+	private UsuarioRepository usuarioRepository;
 	
-	private List<Integer> itens;
+	@Inject
+	private ClienteRepository clienteRepository;
+	
+	@Inject
+	private CadastroPedidoService cadastroPedidoService;
+	
+	private Pedido pedido;
+	private List<Usuario> vendedores;
 	
 	public CadastroPedidoBean() {
+		limpar();
+	}
+	
+	public void inicializar() {
+		if (FacesUtil.isNotPostback()) {
+			this.vendedores = this.usuarioRepository.vendedores();
+		}
+	}
+	
+	private void limpar() {
 		pedido = new Pedido();
 		pedido.setEnderecoEntrega(new EnderecoEntrega());
-		itens = new ArrayList<>();
-		itens.add(1);
 	}
 	
 	public void salvar() {
+		this.pedido = this.cadastroPedidoService.salvar(this.pedido);
+		
+		FacesUtil.addInfoMessage("Pedido salvo com sucesso!");
 	}
-
-	public List<Integer> getItens() {
-		return itens;
+	
+	public FormaPagamento[] getFormasPagamento() {
+		return FormaPagamento.values();
+	}
+	
+	public List<Cliente> completarCliente(String nome) {
+		return this.clienteRepository.porNome(nome);
 	}
 
 	public Pedido getPedido() {
 		return pedido;
+	}
+
+	public List<Usuario> getVendedores() {
+		return vendedores;
 	}
 
 }
