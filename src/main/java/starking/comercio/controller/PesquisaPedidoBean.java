88e514c188ebad785b06d28ea5dfaf956b92a8ca
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-04 17:36:20 -0300

    implementando pedidos e criando class

--- src/main/java/starking/comercio/controller/PesquisaPedidoBean.java
+++ src/main/java/starking/comercio/controller/PesquisaPedidoBean.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+
+import starking.comercio.model.Pedido;
+import starking.comercio.model.enums.StatusPedido;
+import starking.comercio.repository.PedidoRepository;
+import starking.comercio.repository.filter.PedidoFilter;
 
 /**
  * @author pedroRhamon
@@ -14,15 +20,30 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class PesquisaPedidoBean {
 
-	private List<Integer> pedidosFiltrados = new ArrayList<>();
+	@Inject
+	private PedidoRepository repository;
+	
+	private PedidoFilter filtro;
+	private List<Pedido> pedidosFiltrados;
 	
 	public PesquisaPedidoBean() {
-		for(int i = 0; i<50; i++) {
-			pedidosFiltrados.add(i);
-		}
+		filtro = new PedidoFilter();
+		pedidosFiltrados = new ArrayList<>();
+	}
+
+	public void pesquisar() {
+		pedidosFiltrados = this.repository.filtrados(filtro);
 	}
 	
-	public List<Integer> getPedidosFiltrados() {
+	public StatusPedido[] getStatuses() {
+		return StatusPedido.values();
+	}
+	
+	public List<Pedido> getPedidosFiltrados() {
 		return pedidosFiltrados;
+	}
+
+	public PedidoFilter getFiltro() {
+		return filtro;
 	}
 }
