cb73fbb70574487373c070d2bd2c888c30a216db
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-06 17:32:14 -0300

    criando regra de permissao de acordo com o status

--- src/main/java/starking/comercio/model/Pedido.java
+++ src/main/java/starking/comercio/model/Pedido.java
@@ -189,4 +189,14 @@ public class Pedido implements Serializable {
 	public boolean isNaoCancelavel() {
 		return !this.isCancelavel();
 	}
+
+	@Transient
+	public boolean isAlteravel() {
+		return this.isOrcamento();
+	}
+	
+	@Transient
+	public boolean isNaoAlteravel() {
+		return !this.isAlteravel();
+	}
 }
