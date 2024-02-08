1b44be386a95845a0faa64e7f1b7b858f263d43a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-07 23:48:22 -0300

    finalizando tela de grafico

--- src/main/java/starking/comercio/controller/GraficoPedidosCriadosBean.java
+++ src/main/java/starking/comercio/controller/GraficoPedidosCriadosBean.java
@@ -1,5 +1,11 @@
 package starking.comercio.controller;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -8,8 +14,10 @@ import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.ChartSeries;
 
 import lombok.Getter;
+import starking.comercio.model.Usuario;
 import starking.comercio.repository.PedidoRepository;
 import starking.comercio.util.security.UsuarioSistema;
+import starking.comercio.validation.UsuarioLogado;
 
 /**
  * @author pedroRhamon
@@ -20,31 +28,38 @@ import starking.comercio.util.security.UsuarioSistema;
 @Getter
 public class GraficoPedidosCriadosBean {
 	
-	private CartesianChartModel model;
+private static DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM");
 	
 	@Inject
 	private PedidoRepository pedidoRepository;
 	
 	@Inject
+	@UsuarioLogado
 	private UsuarioSistema usuarioLogado;
 	
+	private CartesianChartModel model;
+
 	public void preRender() {
 		this.model = new CartesianChartModel();
 		
-		this.adicionarSerie("Todos os pedidos");
-		this.adicionarSerie("Meus pedidos");
+		adicionarSerie("Todos os pedidos", null);
+		adicionarSerie("Meus pedidos", usuarioLogado.getUsuario());
 	}
-
-	private void adicionarSerie(String string) {
-		ChartSeries series = new ChartSeries();
+	
+	private void adicionarSerie(String rotulo, Usuario criadoPor) {
+		Map<Date, BigDecimal> valoresPorData = this.pedidoRepository.valoresTotaisPorData(15, criadoPor);
 		
-		series.set("1", Math.random() * 1000);
-		series.set("2", Math.random() * 1000);
-		series.set("3", Math.random() * 1000);
-		series.set("4", Math.random() * 1000);
-		series.set("5", Math.random() * 1000);
+		ChartSeries series = new ChartSeries(rotulo);
+		
+		for (Date data : valoresPorData.keySet()) {
+			series.set(DATE_FORMAT.format(data), valoresPorData.get(data));
+		}
 		
 		this.model.addSeries(series);
+	}
+
+	public CartesianChartModel getModel() {
+		return model;
 	}
 
 }
